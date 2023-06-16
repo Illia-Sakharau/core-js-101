4c01ff809b3e2fbc80fdf9a0a653e888149eea18
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-16 16:42:54 +0400

    02-numbers: 6/12

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -110,8 +110,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scalarMult = x1 * x2 + y1 * y2;
+  const aVectorMod = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const bVectorMod = Math.sqrt(x2 ** 2 + y2 ** 2);
+
+  return Math.acos(scalarMult / (aVectorMod * bVectorMod));
 }
 
 /**
