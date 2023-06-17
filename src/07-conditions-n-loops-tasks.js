25e6347ce6e7ca4efe4b516af6abd96a2e8ddf33
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-18 01:09:21 +0400

    07-condition-n-loops: 6/17

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -169,8 +169,10 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const line = Math.sqrt((Math.abs(circle.center.x - point.x)) ** 2
+    + (Math.abs(circle.center.y - point.y)) ** 2);
+  return circle.radius > line;
 }
 
 
