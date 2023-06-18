5aac4d3c8fe29c5baa69fd85202428c8170e08b1
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-18 13:33:14 +0400

    07-condition-n-loops: 11/17

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -280,8 +280,21 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const corEven = String(ccn).length % 2;
+  const lunaSumm = String(ccn)
+    .split('')
+    .reduce((acc, num, ind) => {
+      if ((ind + corEven) % 2 === 0) {
+        const multNum = +num * 2;
+        if (multNum > 9) {
+          return acc + (multNum % 10) + 1;
+        }
+        return acc + multNum;
+      }
+      return acc + +num;
+    }, 0);
+  return lunaSumm % 10 === 0;
 }
 
 /**
