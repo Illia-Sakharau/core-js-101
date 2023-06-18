803378bee073fc635069b793c49a2400aafd202c
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-18 17:28:04 +0400

    07-condition-n-loops: 17/17

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -458,8 +458,14 @@ function getCommonDirectoryPath(pathes) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
+function getMatrixProduct(m1, m2) {
+  const newArr = new Array(m1.length)
+    .fill(new Array(m2[0].length).fill(0))
+    .map((newLine, i) => newLine.map((_, j) => m1[i].reduce((acc, el1, j1) => {
+      const el2 = m2[j1][j];
+      return acc + el1 * el2;
+    }, 0)));
+  return newArr;
 }
 
 
@@ -493,8 +499,22 @@ function getMatrixProduct(/* m1, m2 */) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(position) {
+  for (let i = 0; i < 3; i += 1) {
+    if (position[i][0] && position[i][0] === position[i][1] && position[i][0] === position[i][2]) {
+      return position[i][0];
+    }
+    if (position[0][i] && position[0][i] === position[1][i] && position[0][i] === position[2][i]) {
+      return position[0][i];
+    }
+  }
+  if (position[1][1] && position[0][0] === position[1][1] && position[1][1] === position[2][2]) {
+    return position[1][1];
+  }
+  if (position[1][1] && position[0][2] === position[1][1] && position[1][1] === position[2][0]) {
+    return position[1][1];
+  }
+  return undefined;
 }
 
 
