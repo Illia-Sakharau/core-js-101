6964c7d038d368a0e2c41ba730c0918d7e71cfd6
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-18 18:13:55 +0400

    08-functions-n-closures: 3/8

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -62,8 +62,9 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  const reverseArr = args.reverse();
+  return (x) => reverseArr.reduce((acc, coef, i) => acc + (coef * (x ** i)), 0);
 }
 
 
