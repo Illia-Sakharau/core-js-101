e0caa8d162e86d30bcb32ace3c80aeef3e2fed21
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-18 13:37:57 +0400

    07-condition-n-loops: 12/17

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -311,8 +311,14 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num < 10) {
+    return num;
+  }
+  const summ = String(num)
+    .split('')
+    .reduce((acc, el) => acc + +el, 0);
+  return getDigitalRoot(summ);
 }
 
 
